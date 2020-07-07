4c3633dbb1088a101c346f97642237bf8efb679c
Nick Dambrosio <ndambro303@gmail.com>
2020-07-07 11:58:14 -0600

    add routing links for dashboard

--- SatDash/src/app/dashboard/dashboard.component.ts
+++ SatDash/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../contact';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  contacts: Contact[] = [];
 
-  constructor() { }
+  constructor(private ContactService: ContactService) {}
 
   ngOnInit(): void {
+    this.getContacts();
   }
 
+  // replace this with contacts.flagged === true or something more useful than just the top four contacts
+  getContacts(): void {
+    this.ContactService.getContacts().subscribe(
+      (contacts) => (this.contacts = contacts.slice(0, 4))
+    );
+  }
 }
