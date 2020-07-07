3021735cc9d90ab925d7b38518f391c199967e07
Nick Dambrosio <ndambro303@gmail.com>
2020-07-06 18:32:21 -0600

    create detail component to break up contact

--- SatDash/src/app/app.module.ts
+++ SatDash/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactsComponent],
+  declarations: [AppComponent, ContactsComponent, ContactDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
