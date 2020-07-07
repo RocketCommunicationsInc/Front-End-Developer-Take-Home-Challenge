195c9cfb7bcefd5fa59930ee80f083b46956ad5d
Nick Dambrosio <ndambro303@gmail.com>
2020-07-06 19:44:59 -0600

    add a contact service

---
+++ SatDash/src/app/contact.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../contact';
+import { CONTACTS } from './mock-contacts';
+
+// components shouldnt fetch or save data directly
+// they shouldnt knowingly present fake data, either. they should focus on
+// presenting data and delegate data access to a service
+
+// servics are a great way to share information among classes that don't know each other.
+@Injectable({
+  // accepts metadata just like @Component() does for components
+  // by default, ng generate service registers a provider with the root injector for your service by including provider metadata
+  // when you provide the service at the root level, angular creates a single, shared instance of the HeroService that injects into any class that asks for it.
+
+  providedIn: 'root',
+})
+export class ContactService {
+  getContacts(): Contact[] {
+    return CONTACTS;
+  }
+  constructor() {}
+}
