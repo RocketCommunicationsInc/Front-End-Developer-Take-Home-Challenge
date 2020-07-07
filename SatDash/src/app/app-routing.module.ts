4434e2a3cf54dfd492f58e6045d055d8a3754b74
Nick Dambrosio <ndambro303@gmail.com>
2020-07-07 11:36:22 -0600

    add app routing module and import Contacts component for router

--- SatDash/src/app/app-routing.module.ts
+++ SatDash/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'heroes', component: ContactsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
