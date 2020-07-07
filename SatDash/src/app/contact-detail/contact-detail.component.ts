93829ccb96ccdf91267374e47e455387493d3d3a
Nick Dambrosio <ndambro303@gmail.com>
2020-07-07 13:42:41 -0600

    add routing to the contact detail component

--- SatDash/src/app/contact-detail/contact-detail.component.ts
+++ SatDash/src/app/contact-detail/contact-detail.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../../contact';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { ContactService } from '../contact.service';
+
+// previously, contacts component set the contact for contactDetail. it doesnt do that anymore
+// instead, it creates a route in response to a url
+// contact detail needs a new way to obtain the contact to display.
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
@@ -9,7 +16,14 @@ import { Contact } from '../../contact';
 export class ContactDetailComponent implements OnInit {
   @Input() contact: Contact;
 
-  constructor() {}
+  // ActivatedRoute holds information about the route to this instance of the contactDetailComponent
+  // ContactService gets data from the remote server and this component will use it to display the contact
+  // location is an angular service for interacting with the browser.
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {}
 }
