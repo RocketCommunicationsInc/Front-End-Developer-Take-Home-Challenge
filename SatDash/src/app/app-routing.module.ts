d7b0947cc87431b3f32fac718df61395739fbaab
Nick Dambrosio <ndambro303@gmail.com>
2020-07-07 11:39:36 -0600

    replace app-contacts with router outlet in app.component

--- SatDash/src/app/app-routing.module.ts
+++ SatDash/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
 
-const routes: Routes = [{ path: 'heroes', component: ContactsComponent }];
+// a typical angular route has two properties
+// path: a string that matches url in the browser address bar
+// component: component that the router should create when navigating to this route
+const routes: Routes = [{ path: 'contacts', component: ContactsComponent }];
 
 @NgModule({
+  // add routerModule to the appRoutingMpdule imports array and configure it with routes
   imports: [RouterModule.forRoot(routes)],
+  // export RouterModule so it can be used throughout app
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
