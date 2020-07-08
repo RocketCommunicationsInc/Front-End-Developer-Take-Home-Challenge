a8a1cb85624e8f4f58adb1d01239c863bba13a0d
Nick Dambrosio <ndambro303@gmail.com>
2020-07-08 12:49:51 -0600

    revert inMemoryApi

--- SatDash/src/app/app-routing.module.ts
+++ SatDash/src/app/app-routing.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-// a typical angular route has two properties
-// path: a string that matches url in the browser address bar
-// component: component that the router should create when navigating to this route
 const routes: Routes = [
-  // default route '' to redirect to contacts
   { path: '', redirectTo: 'contacts', pathMatch: 'full' },
-  //parameterized route for contacts, : indicates that :id is a placeholder for a specific contact id
   { path: 'contacts', component: ContactsComponent },
   { path: 'detail/:contactName', component: ContactDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
-  // add routerModule to the appRoutingMpdule imports array and configure it with routes
   imports: [RouterModule.forRoot(routes)],
-  // export RouterModule so it can be used throughout app
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- SatDash/src/app/app.module.ts
+++ SatDash/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
-import { DebuggerComponent } from './debugger/debugger.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DebuggerComponent } from './debugger/debugger.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,16 +22,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   // httpClientInMemoryWebApiModule intercepts Http requests and returns simulated server responses.
   //  I can remove it when a real server is ready to receive requests
 
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-      dataEncapsulation: false,
-    }),
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
