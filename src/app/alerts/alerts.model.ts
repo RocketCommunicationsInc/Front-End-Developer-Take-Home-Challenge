7ba5d67492dde37cfa32d6d3f47a41e189645ae8
Scott <scott.carnett@dvt-inc.com>
2020-12-10 11:21:32 -0600

    model cleanup

--- src/app/alerts/alerts.model.ts
+++ src/app/alerts/alerts.model.ts
@@ -1,33 +1,11 @@
 export class Alert {
-  errorId: string
-  errorSeverity: string
-  errorCategory: string
-  errorMessage: number
-  longMessage: string
-  errorTime: number
-  selected: boolean
-  new: boolean
-  expanded: boolean
-
-  constructor(
-    errorId: string,
-    errorSeverity: string,
-    errorCategory: string,
-    errorMessage: number,
-    longMessage: string,
-    errorTime: number,
-    selected: boolean,
-    _new: boolean,
-    expanded: boolean,
-  ) {
-    this.errorId = errorId
-    this.errorSeverity = errorSeverity
-    this.errorCategory = errorCategory
-    this.errorMessage = errorMessage
-    this.longMessage = longMessage
-    this.errorTime = errorTime
-    this.selected = selected
-    this.new = _new
-    this.expanded = expanded
-  }
+  errorId: string = ''
+  errorSeverity: string = ''
+  errorCategory: string = ''
+  errorMessage: number = 0
+  longMessage: string = ''
+  errorTime: number = 0
+  selected: boolean = false
+  new: boolean = false
+  expanded: boolean = false
 }
