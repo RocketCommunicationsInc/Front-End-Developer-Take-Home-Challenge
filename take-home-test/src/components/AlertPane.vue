d2cdf22689eecc7084935482ba7c7457d1a4c122
chrizzis <christian.d.seabrook@gmail.com>
2022-01-25 18:38:06 -0800

    filter behavior added with default

--- take-home-test/src/components/AlertPane.vue
+++ take-home-test/src/components/AlertPane.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="alert-pane">
     <div class="alert-header">
+      <div class="alert-filters">
+        <!-- TODO: ux tweaks? - [popup menu, scrollable content]  -->
+        <!-- TODO: clear filters button -->
+        <rux-checkbox-group label="Filters">
+          <rux-checkbox
+            v-for="option in filterOptions"
+            :key="option.name"
+            :checked="option.value ? true : false"
+            :name="option.name"
+            :value="option.value"
+            @ruxchange="refilter($event.target)"
+          >
+            {{ option.label }}: {{ option.count }}
+          </rux-checkbox>
+        </rux-checkbox-group>
+      </div>
       <div class="alert-sorts">
-        <!-- TODO: looks like these components just take init and emit, but no v-model (i cant figure out how to make rux-select update its model) -->
         <rux-select
           label="Sort By"
           input-id="2"
@@ -30,6 +45,7 @@
 // TODO: temp - this should be moved closer to backend
 import rawContactsWithAlerts from "../../../data.json";
 import AlertList from "@/components/AlertList";
+
 export default {
   name: "AlertPane",
   components: {
@@ -52,6 +68,8 @@ export default {
       "off",
       // undefined,
     ],
+    filterOptions: [], // key, checked, name, label, count
+    activeFilters: ["new"],
   }),
   created() {
     console.debug(`${this.$options.name}.created`);
@@ -59,9 +77,69 @@ export default {
   },
   watch: {},
   computed: {
-    // TODO: vuex store
+    getFilterOptions() {
+      const alertCounts = this.getFacetedAlertCounts;
+      const useNew = this.activeFilters.includes("new");
+      console.log(alertCounts);
+      const filterOptionsList = ["new"]
+        .concat(this.severityOrder)
+        .map((key) => {
+          const option = {
+            name: key,
+            value: this.activeFilters.includes(key),
+            label: key,
+          };
+          // TODO: eew
+          if (alertCounts[key]?.count) {
+            option.count = useNew
+              ? alertCounts[key].new
+              : alertCounts[key].count;
+          } else {
+            option.count = alertCounts[key];
+          }
+          return option;
+        }, this)
+        .filter((option) => option.count > 0);
+
+      return filterOptionsList;
+    },
+
+    // TODO: all below here are candidates for vuex store
     getAlertList() {
       const sortOrder = this.activeSortOrder;
+      const filters = this.activeFilters;
+      // TODO: optimization - if sort order is changed, can work with existing filtered list
+      // TODO: i think i can make these templates in the store, then add isFiltered to each? i dont think functions can be observable?
+      // TODO: this can be a map: new: {intersect: true, isFiltered... }, then severityOrder.forEach add props
+      const filterOptions = [
+        {
+          key: "new",
+          intersect: true,
+          isFiltered: (alert) => alert.new === true,
+        },
+      ].concat(
+        this.severityOrder.map((status) => {
+          return {
+            key: status,
+            isFiltered: (alert) => alert.errorSeverity === status,
+          };
+        })
+      );
+
+      const filterMap = {
+        new: {
+          intersect: true,
+          // TODO: TEMP - data.json alert.new = false - confusion
+          // TODO: TEMP - there is one per status key
+          isFiltered: (alert) => alert.new === true,
+        },
+      };
+      // disjoint sets
+      this.severityOrder.forEach((status) => {
+        filterMap[status] = {
+          isFiltered: (alert) => alert.errorSeverity === status,
+        };
+      });
       const sortCompareMap = {
         errorTime: (a, b) => {
           if (a.errorTime > b.errorTime) {
@@ -96,6 +174,41 @@ export default {
       };
 
       let facetedAlerts = this.getRawAlerts;
+
+      if (filters.length) {
+        const unionFilters = Object.keys(filterMap)
+          .filter((key) => filters.includes(key) && !filterMap[key].intersect)
+          .reduce((obj, key) => {
+            obj[key] = filterMap[key];
+            return obj;
+          }, {});
+
+        if (Object.entries(unionFilters).length) {
+          facetedAlerts = facetedAlerts.filter((alert) => {
+            // TODO: if this passes the test (true), it is kept
+            // if this is in one of the options, show it - so... array.some
+            // TODO: i think this is a union filter, for intersection, i need to refilter
+            if (
+              Object.keys(unionFilters).some((key) =>
+                unionFilters[key].isFiltered(alert)
+              )
+            ) {
+              return true;
+            }
+            return false;
+          });
+        }
+
+        // NOTE: this solution only works for one intersection filter.
+        const intersectFilter = filterOptions.find(
+          (option) => option.intersect && filters.includes(option.key)
+        );
+        if (intersectFilter) {
+          facetedAlerts = facetedAlerts.filter((alert) =>
+            intersectFilter.isFiltered(alert)
+          );
+        }
+      }
 
       facetedAlerts.sort(sortCompareMap[sortOrder]);
 
@@ -135,6 +248,33 @@ export default {
       });
       return rawAlerts;
     },
+    // TODO: i hope i can consolidate templates for these datastructures, this is getting to be a mess
+    getFacetedAlertCounts() {
+      // TODO: faceted counts affected by intersect facet - this context? calling?
+      // TODO: temp until moved to store - need to apply to unfiltered, then if new, apply new filter to count (fuuuuuuck)
+      const alertList = this.getRawAlerts;
+      const facetCount = {};
+      alertList.forEach((alert) => {
+        // if this alert has a prop in the template
+        const { new: unacknowledged, errorSeverity } = alert;
+        if (unacknowledged) {
+          facetCount.new = facetCount.new ? facetCount.new + 1 : 1;
+        }
+        if (errorSeverity) {
+          if (!facetCount[errorSeverity]) {
+            facetCount[errorSeverity] = {
+              new: 0,
+              count: 0,
+            };
+          }
+          facetCount[errorSeverity].count += 1;
+          if (unacknowledged) {
+            facetCount[errorSeverity].new += 1;
+          }
+        }
+      });
+      return facetCount;
+    },
   },
   methods: {
     toggleSelected(alert) {
@@ -145,9 +285,10 @@ export default {
     },
     init() {
       this.alerts = this.getAlertList;
+      this.filterOptions = this.getFilterOptions;
     },
 
-    // TODO: debating vuex mapMutations('RESORT') which should update alerts getter
+    // TODO: debating vuex mapMutations([RESORT,REFILTER]) which should update alerts getter
     resort(sortOrder) {
       // TODO: more scalable for more sort options
       console.log(`resort: ${sortOrder}`);
@@ -155,6 +296,18 @@ export default {
         return;
       }
       this.activeSortOrder = sortOrder;
+      this.alerts = [...this.getAlertList];
+    },
+    refilter(filter) {
+      const { checked, name } = filter;
+      if (checked) {
+        this.activeFilters.push(name);
+      } else {
+        this.activeFilters.splice(this.activeFilters.indexOf(name), 1);
+      }
+      if (name === "new") {
+        this.filterOptions = [...this.getFilterOptions];
+      }
       this.alerts = [...this.getAlertList];
     },
   },
