bc8fcf1562e23cd46cc9990d72a9eafdbc36bc2c
chrizzis <christian.d.seabrook@gmail.com>
2022-01-25 16:07:29 -0800

    add astro resources and stub component

--- take-home-test/src/App.vue
+++ take-home-test/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <AlertPane />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlertPane from "@/components/AlertPane.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AlertPane,
+  },
+};
 </script>
 
 <style>
+/* TODO: themes take over general styling
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- take-home-test/src/main.js
+++ take-home-test/src/main.js
@@ -1,8 +1,24 @@
+// main.js
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Import Astro's base styles
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css'
+
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@astrouxds/astro-web-components/loader'
+
+applyPolyfills().then(() => {
+  defineCustomElements()
+})
+
+// Tell Vue to ignore all components defined in the astro-web-components package
+Vue.config.ignoredElements = [/rux-\w*/]
+
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
