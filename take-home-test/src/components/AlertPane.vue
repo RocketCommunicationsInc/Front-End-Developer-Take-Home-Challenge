326e4df62e9851c97f5c623dfd7ae6b1cc0deca0
chrizzis <christian.d.seabrook@gmail.com>
2022-01-25 17:09:35 -0800

    AlertList surfacing alerts from data.json

--- take-home-test/src/components/AlertPane.vue
+++ take-home-test/src/components/AlertPane.vue
@@ -8,6 +8,8 @@
   </div>
 </template>
 <script>
+// TODO: temp - this should be moved closer to backend
+import rawContactsWithAlerts from "../../../data.json";
 import AlertList from "@/components/AlertList";
 export default {
   name: "AlertPane",
@@ -15,35 +17,66 @@ export default {
     AlertList,
   },
   data: () => ({
-    alerts: [
-      {
-        // TODO: there are duplicate ids - need to uniquify
-        errorId: "d3472160-9d02-5f20-95ee-772c02a6233a",
-        errorTime: 1542134427946,
-        errorSeverity: "critical",
-        errorMessage: "Red FEP 124 - Offline",
-        contactTime: "cBTs-cETs",
-        contactName: 64900,
-        // TODO: this is false for all alerts - need to transform raw
-        new: true,
-        // TODO: rux-modal - from contact (parent) context - i can either wire a getter to get the props from the parent for each alert, OR alertList getter should add them to the alert, but duped data...
-        contactSatellite: "USA-256",
-        contactDetail:
-          "Lorem esse proident exercitation ad commodo fugiat consectetur laborum.",
-      },
-    ],
+    alerts: [],
   }),
   created() {
     console.debug(`${this.$options.name}.created`);
+    this.init();
   },
   watch: {},
-  computed: {},
+  computed: {
+    // TODO: move this to the store
+    // TODO: when moved to the store, remove params - store will manage sorts/filters
+    getAlertList() {
+      let facetedAlerts = this.getRawAlerts;
+
+      // TODO: apply mutators
+
+      // TODO: should this transform data (timestamp maths) to consumer or an alert component?
+      return facetedAlerts;
+    },
+    // TODO: this should be stored so i dont have to reprocess with different random numbers
+    getRawAlerts() {
+      // TODO: rawAlerts will emulate the api response
+      let rawAlerts = rawContactsWithAlerts
+        .map((contact) =>
+          contact.alerts.map((alert) => ({
+            // contactId: contact.contactId, // I could just add the contact data directly...
+            contactName: contact.contactName,
+            contactDetail: contact.contactDetail,
+            ...alert,
+          }))
+        )
+        .reduce((acc, curVal) => acc.concat(curVal), []);
+
+      // tweak raw data for a richer dataset
+      let severityChanged = {};
+      rawAlerts = rawAlerts.map((alert) => {
+        const randomized = Math.floor(100 + Math.random() * 900);
+        alert.errorId = alert.errorId.concat(randomized);
+        const changedCount = severityChanged[alert.errorSeverity];
+        if (changedCount) {
+          if (changedCount > 2) {
+            return alert;
+          }
+          severityChanged[alert.errorSeverity] += 1;
+          return { ...alert, new: true };
+        }
+        severityChanged[alert.errorSeverity] = 1;
+        return { ...alert, new: true };
+      });
+      return rawAlerts;
+    },
+  },
   methods: {
     toggleSelected(alert) {
       console.log(`toggleSelected: ${alert}`);
     },
     showDetails(alert) {
       console.log(`showDetails: ${alert}`);
+    },
+    init() {
+      this.alerts = this.getAlertList;
     },
   },
 };
