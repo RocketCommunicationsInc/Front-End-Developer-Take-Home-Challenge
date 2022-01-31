24ba30762c91cc98d0cf518bdf5576943cbf4156
nicolewallace09 <nicole.elisaw@gmail.com>
2022-01-31 11:33:10 -0800

    last commit before submission

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,8 +7,7 @@ import {
     RuxTableCell,
     RuxButton,
     RuxSelect,
-    RuxOption,
-    RuxCheckbox
+    RuxOption
 } from '@astrouxds/react' 
 import { useState, useMemo } from 'react'; 
 import contacts from '../data.json';
@@ -19,9 +18,9 @@ import TableCell from './TableCells';
 const Dashboard = () => {
     const [openModal, setOpenModal] = useState(false); 
     const [data, setData] = useState([]); 
-    const [checked, setChecked] = useState(false); 
     const [value, setValue] = useState('All'); 
-
+     
+    
     // filtering by contacts that contact alerts 
     let newContacts = contacts.filter((data) => data.alerts.length > 0)
     
@@ -36,11 +35,18 @@ const Dashboard = () => {
         return newContacts.filter(item => item.alerts[0].errorSeverity === value)
     }, [value, newContacts]);
 
-    const handleCheckbox = (event) => {
-        setChecked(event.target.value); 
-    }
+    const [checked, setChecked] = useState(new Array(newContacts.length).fill(false));
 
-    console.log(checked)
+    const handleCheckbox = (position) => {
+        const updatedCheckedState = checked.map((item, index) => {
+            if (index === position) {
+                return !item;
+            } else {
+                return item;
+            }
+        });
+        setChecked(updatedCheckedState);
+    };
 
     return (
         <>
@@ -49,6 +55,7 @@ const Dashboard = () => {
                     <RuxTableRow>
                         <TableHeader/>
                         <RuxTableHeaderCell>
+                        <span>Filter By:</span>
                         <RuxSelect value={value} onMouseLeave={(e) => setValue(e.target.value)}>
                                 <RuxOption 
                                     value=''
@@ -73,33 +80,32 @@ const Dashboard = () => {
                     </RuxTableRow>
                 </RuxTableHeader>
                 <RuxTableBody>
-                {filteredContacts.map((contact) => { 
-                    return (
-                        <RuxTableRow selected={checked === contact._id ? '' : 'false'}>
-                            <TableCell data={contact} key={contact._id}/>
-                            <RuxTableCell>
-                                <RuxButton
-                                    onClick={() => {
-                                        setData(contact);
-                                        setOpenModal(true);
-                                    }}
-                                >
-                                Show Details
-                                </RuxButton>
-                            </RuxTableCell> 
-                            <RuxTableCell>
-                                <RuxCheckbox
-                                 type="checkbox" 
-                                 className="acknowledged" 
-                                 value={contact._id}
-                                 onClick={handleCheckbox}
-                                 key={contact._id}
-                                >
-                                </RuxCheckbox>
-                            </RuxTableCell>
-                        </RuxTableRow>
-                )})}
-            </RuxTableBody> 
+                    {filteredContacts.map((contact, name) => { 
+                        return (
+                            <RuxTableRow key={name} selected={checked[name] === true ? '' : 'false'}>
+                                <TableCell data={contact}/>
+                                <RuxTableCell>
+                                    <RuxButton
+                                        onClick={() => {
+                                            setData(contact);
+                                            setOpenModal(true);
+                                        }}
+                                    >
+                                    Show Details
+                                    </RuxButton>
+                                </RuxTableCell> 
+                                <RuxTableCell>
+                                    <input 
+                                        type="checkbox" 
+                                        value={contacts._id}
+                                        name={contacts._id}
+                                        onChange={() => handleCheckbox(name)}
+                                        checked={checked[name]}
+                                    />                               
+                                </RuxTableCell>
+                            </RuxTableRow>
+                    )})}
+                </RuxTableBody> 
             </RuxTable>
             {openModal && <Modal openModal={openModal} setOpenModal={setOpenModal} data={data}/>}
         </>
@@ -108,9 +114,3 @@ const Dashboard = () => {
 
 export default Dashboard; 
 
-{/* <input 
-type="checkbox" 
-className="acknowledged" 
-value={contact._id}
-onChange={handleCheckbox}
-/> */}
