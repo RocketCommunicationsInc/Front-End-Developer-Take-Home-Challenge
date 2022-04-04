7b47744135fde633fc6762d620b4c2288cec7eee
lsprr <16653744+lsprr@users.noreply.github.com>
2022-04-04 17:43:56 -0400

    Eliminate unnecessary codes

--- src/components/CommandCenter.js
+++ src/components/CommandCenter.js
@@ -1,17 +1,14 @@
 import data from '../data.json'
-import { useState } from "react";
 import { Header } from "./Header";
 import Table from "./Table";
 
 export const CommandCenter = () => {
 
-    const [rows, setRows] = useState(data);
-
     /* 
         WARNING: THIS CODE OF PIECE MUTATES THE ORIGINAL DATA.
         Calls a function for each row in rows, "detect" undeclared array in alerts, and then add the empty detail onto the undeclared array  
     */
-    rows.forEach((row) => {
+    data.forEach((row) => {
         if (typeof row.alerts[0] === 'undefined') {
             row.alerts.push({
                 "errorId": "",
@@ -28,12 +25,12 @@ export const CommandCenter = () => {
     });
 
     /* The alerts to be sorted by error time with the most recent at the top */
-    rows.sort((a, b) => (a.alerts[0].errorTime < b.alerts[0].errorTime ? 1 : -1));
+    data.sort((a, b) => (a.alerts[0].errorTime < b.alerts[0].errorTime ? 1 : -1));
 
     return (
         <>
             <Header />
-            <Table data={rows} />
+            <Table data={data} />
         </>
     )
 }
