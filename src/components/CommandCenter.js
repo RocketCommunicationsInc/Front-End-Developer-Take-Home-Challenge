1cd3585ee5c48b7d57f470e52bab5039ab2f6fb4
lsprr <16653744+lsprr@users.noreply.github.com>
2022-04-05 13:06:33 -0400

    Eliminate mutated code

--- src/components/CommandCenter.js
+++ src/components/CommandCenter.js
@@ -3,12 +3,9 @@ import { Header } from "./Header";
 import Table from "./Table";
 
 export const CommandCenter = () => {
+    const deepClone = JSON.parse(JSON.stringify(data));
 
-    /* 
-        WARNING: THIS CODE OF PIECE MUTATES THE ORIGINAL DATA.
-        Calls a function for each row in rows, "detect" undeclared array in alerts, and then add the empty detail onto the undeclared array  
-    */
-    data.forEach((row) => {
+    deepClone.forEach((row) => {
         if (typeof row.alerts[0] === 'undefined') {
             row.alerts.push({
                 "errorId": "",
@@ -25,12 +22,12 @@ export const CommandCenter = () => {
     });
 
     /* The alerts to be sorted by error time with the most recent at the top */
-    data.sort((a, b) => (a.alerts[0].errorTime < b.alerts[0].errorTime ? 1 : -1));
+    deepClone.sort((a, b) => (a.alerts[0].errorTime < b.alerts[0].errorTime ? 1 : -1));
 
     return (
         <>
             <Header />
-            <Table data={data} />
+            <Table data={deepClone} />
         </>
     )
 }
