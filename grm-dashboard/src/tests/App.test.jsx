9cfad8d7e4893e96ef849a77381cf006ce99cace
sharpie6436 <sharpie6436@email.drews.computer>
2025-01-29 20:58:20 -0700

    Make sure we get an object from data json

--- grm-dashboard/src/tests/App.test.jsx
+++ grm-dashboard/src/tests/App.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect } from "vitest";
+import { it, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
@@ -6,4 +6,10 @@ it("renders the App component", () => {
   render(<App />);
   const pageTitle = screen.getByText(/GRM Dashboard/i);
   expect(pageTitle).toBeInTheDocument();
+});
+
+test("data is valid json", () => {
+  const data = require("../../../data.json");
+  expect(typeof data).toBe("object");
+  expect(data.length).toBeGreaterThan(0);
 });
