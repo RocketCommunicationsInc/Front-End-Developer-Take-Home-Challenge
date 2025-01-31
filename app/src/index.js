500e32142589214f943246f976cf77e201f07c11
Jessica Lynch <lehualani.lynch@gmail.com>
2025-01-31 15:07:19 -0700

    import astro
    Signed-off-by: Jessica Lynch <lehualani.lynch@gmail.com>

--- app/src/index.js
+++ app/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AstroProvider } from '@pingux/astro';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AstroProvider>
+      <App />
+    </AstroProvider>
   </React.StrictMode>
 );
 
