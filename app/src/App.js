cd7874eff2853549ca40eb970bc0850948cd9bed
Jessica Lynch <lehualani.lynch@gmail.com>
2025-02-02 12:13:38 -0700

    cleanup - uninstall pingux astro lib
    Signed-off-by: Jessica Lynch <lehualani.lynch@gmail.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
 
-import { AstroProvider } from '@pingux/astro';
-
 import DashboardComponent from './components/DashboardComponent/DashboardComponent';
 
 function App() {
   return (
     <div className="App">
-      <AstroProvider>
-        <DashboardComponent />
-      </AstroProvider>
+      <DashboardComponent />
     </div>
   );
 }
