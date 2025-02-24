12377072a4ff91ff1689d9742d10c2234c1434c9
Gwen Wendell <gwen@thecodingdesigner.com>
2025-02-24 11:38:11 -0500

    🛠️ AlertViewer.js -> Added tabbed view using Rux components

--- src/app/components/AlertViewer/AlertViewer.js
+++ src/app/components/AlertViewer/AlertViewer.js
@@ -1,7 +1,46 @@
+'use client'
+
 import React from 'react';
+import {
+  RuxContainer,
+  RuxTab,
+  RuxTabs,
+  RuxTabPanel,
+  RuxTabPanels,
+} from "@astrouxds/react";
 
 import styles from './AlertViewer.module.css';
 
-export function AlertViewer({prop = 'default value'}) {
-  return <div className={styles.AlertViewer}>AlertViewer {prop}</div>;
+export function AlertViewer() {
+  return (
+
+      <RuxContainer
+      >
+        <header slot="header">Alerts</header>
+        <RuxTabs slot="tab-bar" id="tab-set-1" small={true}>
+          <RuxTab id="tab-id-1" selected={true}>Grid</RuxTab>
+          <RuxTab id="tab-id-2">List</RuxTab>
+        </RuxTabs>
+        <div slot="toolbar">
+          <span>30 Total</span>
+          <span>|</span>
+          <span>Filters</span>
+          <span>20 New</span>
+          <span>10 Acknowledged</span>
+          <span>5 Critical</span>
+          <span>2 Serious</span>
+          <span>2 Caution</span>
+          <span>2 Warning</span>
+          <span>Clear Filters</span>
+        </div>
+        <RuxTabPanels aria-labelledby="tab-set-1">
+          <RuxTabPanel aria-labelledby="tab-id-1">
+            <p>tag set grid</p>
+          </RuxTabPanel>
+          <RuxTabPanel aria-labelledby="tab-id-2">
+            <p>tag set list</p>
+          </RuxTabPanel>
+        </RuxTabPanels>
+      </RuxContainer>
+  );
 }
