7693db4788b04d02e21138a6e4cca9e7682b4be9
Gwen Wendell <gwen@thecodingdesigner.com>
2025-02-25 18:02:10 -0500

    🛠️ AlertViewer.jsx -> Refactored code format 🛠️ AlertViewer.jsx -> Removed unused imports 🛠️ AlertViewer.jsx -> Cleaned up console logs 🛠️ AlertViewer.jsx -> Simplified function logic 🛠️ AlertViewer.module.css -> Removed .Orientation class

--- src/app/components/AlertViewer/AlertViewer.jsx
+++ src/app/components/AlertViewer/AlertViewer.jsx
@@ -1,12 +1,8 @@
-"use client";
+"use client"
 
 import React, { useState } from "react";
 import {
   RuxContainer,
-  RuxTab,
-  RuxTabs,
-  RuxTabPanel,
-  RuxTabPanels,
 } from "@astrouxds/react";
 import { AlertLog } from "../AlertLog";
 
@@ -14,18 +10,23 @@ import data from "../../data/data.json";
 
 import styles from "./AlertViewer.module.css";
 
-function daysInYear(date){
-  return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+function daysInYear(date) {
+  return (
+    (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+      Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000
+  );
 }
 
 const formatDate = (date, milliseconds = true) => {
-  const millisecondsMultiplier = (milliseconds) ? 1000 : 1;
+  const millisecondsMultiplier = milliseconds ? 1000 : 1;
   const thisDate = new Date(date * millisecondsMultiplier);
   const options = {
-    timeZone : "UTC",
+    timeZone: "UTC",
     hour12: false,
-  }
-  const dateString = `${thisDate.getUTCFullYear()} ${daysInYear(thisDate)} ${thisDate.toLocaleTimeString('en-US', options)}`
+  };
+  const dateString = `${thisDate.getUTCFullYear()} ${daysInYear(
+    thisDate
+  )} ${thisDate.toLocaleTimeString("en-US", options)}`;
   return dateString;
 };
 
@@ -38,7 +39,10 @@ const transformData = (data) => {
       const thisAlert = alert;
       thisAlert.contactId = item.contactId;
       thisAlert.errorTimeString = formatDate(alert.errorTime, false);
-      item.earliestAlertErrorTime = (alert.errorTime - item.earliestAlertErrorTime)? alert.errorTime : item.earliestAlertErrorTime;
+      item.earliestAlertErrorTime =
+        alert.errorTime - item.earliestAlertErrorTime
+          ? alert.errorTime
+          : item.earliestAlertErrorTime;
     });
   });
 
@@ -51,34 +55,49 @@ const transformData = (data) => {
 
 const getAlertCounts = (data, severity = null) => {
   let count = 0;
-  data.filter((item) => item.alerts.length > 0)
-  console.log(['data', data[0]])
+  data.filter((item) => item.alerts.length > 0);
+  console.log(["data", data[0]]);
   data.map((item, index) => {
     if (severity) {
-      count += item.alerts.filter((alert) => alert.errorSeverity === severity && alert.acknowledged !== true).length
+      count += item.alerts.filter(
+        (alert) =>
+          alert.errorSeverity === severity && alert.acknowledged !== true
+      ).length;
     } else {
-      count += item.alerts.filter((alert) => alert.acknowledged !== true).length
+      count += item.alerts.filter(
+        (alert) => alert.acknowledged !== true
+      ).length;
     }
-  })
-  console.log([severity, count])
+  });
+  console.log([severity, count]);
   return count;
-}
+};
 
 export function AlertViewer() {
   const [alertData, setAlertData] = useState(transformData(data));
-  const [unacknowledgedCount, setUnacknowledgedCount] = useState(getAlertCounts(data));
-  const [criticalCount, setCriticalCount] = useState(getAlertCounts(data, 'critical'));
-  const [seriousCount, setSeriousCount] = useState(getAlertCounts(data, 'serious'));
-  const [cautionCount, setCautionCount] = useState(getAlertCounts(data, 'caution'));
-  const [warningCount, setWarningCount] = useState(getAlertCounts(data, 'warning'));
+  const [unacknowledgedCount, setUnacknowledgedCount] = useState(
+    getAlertCounts(data)
+  );
+  const [criticalCount, setCriticalCount] = useState(
+    getAlertCounts(data, "critical")
+  );
+  const [seriousCount, setSeriousCount] = useState(
+    getAlertCounts(data, "serious")
+  );
+  const [cautionCount, setCautionCount] = useState(
+    getAlertCounts(data, "caution")
+  );
+  const [warningCount, setWarningCount] = useState(
+    getAlertCounts(data, "warning")
+  );
 
   const getAllAlertCounts = (data) => {
     setUnacknowledgedCount(getAlertCounts(data));
-    setCriticalCount(getAlertCounts(data, 'critical'));
-    setSeriousCount(getAlertCounts(data, 'serious'));
-    setCautionCount(getAlertCounts(data, 'caution'));
-    setWarningCount(getAlertCounts(data, 'warning'));
-  }
+    setCriticalCount(getAlertCounts(data, "critical"));
+    setSeriousCount(getAlertCounts(data, "serious"));
+    setCautionCount(getAlertCounts(data, "caution"));
+    setWarningCount(getAlertCounts(data, "warning"));
+  };
 
   const updateOneEntry = ({ alert, contact }) => {
     if (alert.errorId) {
@@ -95,39 +114,21 @@ export function AlertViewer() {
         }
       });
       setAlertData(alertDataCopy);
-      getAllAlertCounts(alertDataCopy)
+      getAllAlertCounts(alertDataCopy);
     }
   };
 
   return (
     <RuxContainer>
       <header slot="header">Alerts</header>
-      <RuxTabs slot="tab-bar" id="tab-set-1" small={true}>
-        <RuxTab id="tab-id-1">Grid</RuxTab>
-        <RuxTab id="tab-id-2" selected={true}>
-          List
-        </RuxTab>
-      </RuxTabs>
       <div slot="toolbar" className={styles.Toolbar}>
-        <span className={styles.Orientation}>Alerts: </span>
-        {/* <span>|</span> */}
-        {/* <span>Filters</span> */}
-        {/* <span>20 New</span> */}
         <span>{unacknowledgedCount} Unacknowledged</span>
         <span>{criticalCount} Critical</span>
         <span>{seriousCount} Serious</span>
         <span>{cautionCount} Caution</span>
         <span>{warningCount} Warning</span>
-        {/* <span>Clear Filters</span> */}
       </div>
-      <RuxTabPanels aria-labelledby="tab-set-1">
-        <RuxTabPanel aria-labelledby="tab-id-1">
-          <p>tag set grid</p>
-        </RuxTabPanel>
-        <RuxTabPanel aria-labelledby="tab-id-2">
-          <AlertLog data={alertData} doUpdateEntry={updateOneEntry} />
-        </RuxTabPanel>
-      </RuxTabPanels>
+      <AlertLog data={alertData} doUpdateEntry={updateOneEntry} />
     </RuxContainer>
   );
 }
