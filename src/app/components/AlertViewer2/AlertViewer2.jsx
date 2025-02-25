5eb258903abc71ee81fca3fe15d4b987c9b62fb2
Gwen Wendell <gwen@thecodingdesigner.com>
2025-02-24 23:09:43 -0500

    🛠️ AlertViewer2.jsx -> Refactored data handling, added functions

--- src/app/components/AlertViewer2/AlertViewer2.jsx
+++ src/app/components/AlertViewer2/AlertViewer2.jsx
@@ -1,26 +1,62 @@
 "use client";
 
 import React, { useState } from "react";
-import {
-  RuxContainer,
-  RuxTab,
-  RuxTabs,
-  RuxTabPanel,
-  RuxTabPanels,
-} from "@astrouxds/react";
+import { RuxContainer } from "@astrouxds/react";
 import { AlertLog2 } from "../AlertLog2";
 
 import data from "../../data/data.json";
 
-import styles from './AlertViewer2.module.css';
+import styles from "./AlertViewer2.module.css";
 
-export function AlertViewer2({prop = 'default value'}) {
-  const [alertData, setAlertData] = useState(data);
+const transformData = (data) => {
+  let justAlerts = [];
+
+  data.map((item, index) => {
+    item.contactBeginTimestampString = new Date(
+      item.contactBeginTimestamp * 1000
+    ).toLocaleString();
+    item.contactEndTimestampString = new Date(
+      item.contactEndTimestamp * 1000
+    ).toLocaleString();
+    item.earliestAlertErrorTime = null;
+    item.alerts.forEach((alert) => {
+      const thisAlert = alert;
+      thisAlert.errorTimeString = new Date(alert.errorTime).toLocaleString();
+      justAlerts.push(alert);
+    });
+  });
+  justAlerts = justAlerts.sort((a, b) => {
+    return a.errorTime - b.errorTime;
+  });
+  // return justAlerts;
+  return data;
+};
+
+export function AlertViewer2() {
+  const [alertData, setAlertData] = useState(transformData(data));
+
+  const updateOneEntry = ({ alert, contact }) => {
+    if (alert.errorId) {
+      const alertDataCopy = alertData.map((item) => {
+        if (contact.contactId === item.contactId) {
+          item.alerts.forEach((a) => {
+            if (a.errorId === alert.errorId) {
+              a.acknowledged = true;
+            }
+          });
+          return item;
+        } else {
+          return item;
+        }
+      });
+      setAlertData(alertDataCopy);
+    }
+  };
 
   return (
     <RuxContainer>
-          <header slot="header">Alerts</header>
-              <AlertLog2 data={alertData}  />
-        </RuxContainer>
+      <header slot="header">Alerts</header>
+      <AlertLog2 data={alertData} doUpdateEntry={updateOneEntry} />
+    </RuxContainer>
   );
 }
