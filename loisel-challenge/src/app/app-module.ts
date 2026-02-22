352b6f71cb2afb036e7c15257e362ce8cc069f4e
Ezra Loisel <efloisel@gmail.com>
2026-02-21 19:41:17 -0700

    Basic sorting added

--- loisel-challenge/src/app/app-module.ts
+++ loisel-challenge/src/app/app-module.ts
@@ -9,10 +9,10 @@ import { AstroComponentsModule } from '@astrouxds/angular';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { AlertCard } from './alert/alert-card';
-
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   declarations: [App, AlertCard],
-  imports: [BrowserModule, AppRoutingModule, AstroComponentsModule],
+  imports: [BrowserModule, AppRoutingModule, AstroComponentsModule, MatSortModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- loisel-challenge/src/app/app.ts
+++ loisel-challenge/src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
 // import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import data from '../../public/data_processed.json';
+import importData from '../../public/data_processed.json';
 import { Alert } from './alert/alert-card';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,30 @@ import { Alert } from './alert/alert-card';
 })
 export class App {
   protected readonly title = signal('loisel-challenge');
-  alerts: Alert[] = data;
+  sortedAlerts: Alert[] = importData.slice();
   constructor() {}
+
+  sortAlerts(sort: Sort) {
+    const data = importData.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedAlerts = data;
+      return;
+    }
+
+    this.sortedAlerts = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'contactName':
+          return compare(a.contactName, b.contactName, isAsc);
+        case 'contactBeginTimestamp':
+          return compare(a.contactBeginTimestamp, b.contactBeginTimestamp, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
